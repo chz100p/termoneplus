73989e4f5b199a337eee560caca41588f07fdd80
Roumen Petrov <maintainer@termoneplus.com>
2021-12-24 19:05:44 +0200

    add own telnet view to lint happy

---
+++ samples/telnet/src/main/java/com/termoneplus/sample/telnet/TelnetView.java
@@ -0,0 +1,22 @@
+package com.termoneplus.sample.telnet;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import jackpal.androidterm.emulatorview.EmulatorView;
+
+
+public class TelnetView extends EmulatorView {
+
+    public TelnetView(Context context) {
+        super(context);
+    }
+
+    public TelnetView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TelnetView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
