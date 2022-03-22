804f576932746442ee5f5eb89ccb071bdd98de9c
Roumen Petrov <maintainer@termoneplus.com>
2022-03-22 19:29:49 +0200

    remove unused ColorScheme argument value
    Remark: TermView function updatePrefs was always called with scheme set to null.

--- term/src/main/java/jackpal/androidterm/TermView.java
+++ term/src/main/java/jackpal/androidterm/TermView.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 Steven Luo
+ * Copyright (C) 2022 Roumen Petrov.  All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +20,6 @@ package jackpal.androidterm;
 import android.content.Context;
 import android.util.DisplayMetrics;
 
-import jackpal.androidterm.emulatorview.ColorScheme;
 import jackpal.androidterm.emulatorview.EmulatorView;
 import jackpal.androidterm.emulatorview.TermSession;
 import jackpal.androidterm.util.TermSettings;
@@ -30,24 +30,16 @@ public class TermView extends EmulatorView {
         super(context, session, metrics);
     }
 
-    public void updatePrefs(TermSettings settings, ColorScheme scheme) {
-        if (scheme == null) {
-            scheme = settings.getColorScheme();
-        }
-
+    public void updatePrefs(TermSettings settings) {
         setTextSize(settings.getFontSize());
+        setColorScheme(settings.getColorScheme());
         setUseCookedIME(settings.useCookedIME());
-        setColorScheme(scheme);
         setBackKeyCharacter(settings.getBackKeyCharacter());
         setAltSendsEsc(settings.getAltSendsEscFlag());
         setControlKeyCode(settings.getControlKeyCode());
         setFnKeyCode(settings.getFnKeyCode());
         setTermType(settings.getTermType());
         setMouseTracking(settings.getMouseTrackingFlag());
-    }
-
-    public void updatePrefs(TermSettings settings) {
-        updatePrefs(settings, null);
     }
 
     @Override
