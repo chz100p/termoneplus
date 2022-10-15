1addf1b3859b5a4c6a8d113b3baa993118b2a4bb
Roumen Petrov <maintainer@termoneplus.com>
2022-10-15 19:07:16 +0300

    toggle soft-input compatibility with Android API 31-33

--- term/src/main/java/com/termoneplus/compat/SoftInputCompat.java
+++ term/src/main/java/com/termoneplus/compat/SoftInputCompat.java
@@ -17,6 +17,9 @@
 package com.termoneplus.compat;
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.os.Build;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
@@ -28,6 +31,46 @@ public class SoftInputCompat {
         InputMethodManager imm = (InputMethodManager)
                 context.getSystemService(Context.INPUT_METHOD_SERVICE);
 
-        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+        // TODO: to use only API 31-33 compatible code
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S /*API level 31*/)
+            Compat.toggle(imm);
+        else
+            Compat31.toggle(view, imm);
+    }
+
+    private static boolean isSoftInputVisible(View view) {
+        View root = view.getRootView();
+
+        Rect r = new Rect();
+        root.getWindowVisibleDisplayFrame(r);
+        /* "root view" height - status bar height (r.top) - visible frame height (r.bottom - r.top) */
+        int delta = root.getBottom() - r.bottom;
+
+        /* threshold size: dp to pixels, multiply with display density */
+        DisplayMetrics metrics = root.getResources().getDisplayMetrics();
+        /* 128dp = 32dp * 4, minimum button height 32dp and generic 4 rows soft keyboard */
+        return delta > 128 /*threshold*/ * metrics.density;
+    }
+
+
+    private static class Compat {
+        @SuppressWarnings({"deprecation", "RedundantSuppression"})
+        private static void toggle(InputMethodManager imm) {
+            // Toggle method was deprecated in API level 31.
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+        }
+    }
+
+    private static class Compat31 {
+        private static void toggle(View view, InputMethodManager imm) {
+            if (isSoftInputVisible(view)) {
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            } else {
+                if (!view.requestFocus())
+                    return;
+                // NOTE: SHOW_FORCED was deprecated in API level 33.
+                imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+            }
+        }
     }
 }
